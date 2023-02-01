6255ab4fafa84124f189ade4f46e9b0e556e9e26
Dmitry Volkov <hyardlung@gmail.com>
2023-02-01 22:46:47 +0700

    feat: add AuthorsView

--- src/views/AuthorsView.vue
+++ src/views/AuthorsView.vue
@@ -1,5 +1,71 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main>
+    <div class="authors" v-if="!isLoading">
+      <div class="author" v-for="author in authorSet" :key="author">
+        <span class="author__name">{{ author.name }}</span>
+        <div class="author__dates">
+          <span class="author__dob">{{ author.birth_year }}</span> —
+          <span class="author__dod">{{ author.death_year }}</span>
+        </div>
+      </div>
+    </div>
+    <div v-else class="loader">
+      <img :src="loader" alt="" class="loader__img" />
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref, onBeforeMount } from "vue";
+
+import { getBooksFromApi } from "../helpers";
+import { books } from "../stores/books";
+import loader from "@/assets/ball-triangle.svg";
+
+onBeforeMount(() => {
+  getBooks();
+  createAuthorSet(books.value);
+});
+
+const isLoading = ref(false);
+const authorSet = new Set();
+
+async function getBooks() {
+  isLoading.value = true;
+  await getBooksFromApi(books);
+  isLoading.value = false;
+}
+
+async function createAuthorSet(array) {
+  await array.forEach((obj) => {
+    obj.authors.forEach((author) => {
+      authorSet.add(author);
+    });
+  });
+}
+</script>
+
+<style lang="sass" scoped>
+.authors
+  display: flex
+  flex-direction: column
+  gap: 40px
+
+.author
+  display: flex
+  flex-direction: column
+  gap: 6px
+  &__name
+    font-size: 28px
+    line-height: 28px
+
+.loader
+  position: relative
+  height: 75vh
+  &__img
+    position: absolute
+    top: 50%
+    right: 50%
+    transform: translate(50%, -50%)
+    display: block
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
-    <RouterLink to="/books" class="books-wrap">
+    <RouterLink to="/books" class="books-wrap books-wrap_books">
       <div class="books-wrap__overlay">Books</div>
     </RouterLink>
     <div></div>
     <div class="divider"></div>
-    <div class="authors"></div>
+    <RouterLink to="/authors" class="books-wrap books-wrap_authors">
+      <div class="books-wrap__overlay">Authors</div>
+    </RouterLink>
   </div>
 </template>
 <script setup></script>
@@ -16,6 +18,7 @@
   height: 100%
   display: grid
   grid-template: 1fr / 1fr auto 1fr
+  grid-template-areas: "books divider authors"
   gap: 20px
 
 .books-wrap
@@ -23,12 +26,14 @@
   font-size: 40px
   font-weight: 700
   color: white
-  background-image: url("@/assets/books.png")
   background-position: top
   background-size: 100%
   transition: all .3s ease
   &:hover
+    font-size: 60px
     background-size: 120%
+  &:hover &__overlay
+    padding: 60px
 
   &__overlay
     position: absolute
@@ -38,9 +43,19 @@
     left: 0
     padding: 30px
     background-color: rgba(0, 0, 0, .5)
+    transition: all .3s ease
     pointer-events: none
 
+  &_books
+    grid-area: books
+    background-image: url("@/assets/books.png")
+
+  &_authors
+    grid-area: authors
+    background-image: url("@/assets/authors.png")
+
 .divider
+  grid-area: divider
   width: 2px
   background-color: #c1c1c1
   border-radius: 100%
