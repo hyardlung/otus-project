575a964376a2c6d934921a629219c245ace5f4cc
Dmitry Volkov <hyardlung@gmail.com>
2023-02-17 08:42:48 +0700

    fix: add placeholder for the book cover

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,9 @@
 export function getImageSrc(obj) {
   const src = obj.formats["image/jpeg"];
-  return src ? src : "";
+  return src ? src : "./src/assets/book-cover-placeholder.svg";
 }
 
 export function strToArr(str) {
+  if (!str) return;
   return str.includes(",") ? str.split(", ") : [str];
 }
